0a9a94309f85c76030a290617ac7a214377669c6
dididthat <diananikulina00@mail.ru>
2024-06-17 15:22:06 +0300

    added .gitignore

---
+++ toDoL/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  toDoL
+//
+//  Created by snydia on 17.06.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ toDoL/toDoLApp.swift
@@ -0,0 +1,17 @@
+//
+//  toDoLApp.swift
+//  toDoL
+//
+//  Created by snydia on 17.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct toDoLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
