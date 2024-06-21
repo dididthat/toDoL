069e1afde540d6e030f790ab3db0b777794cc2b0
dididthat <diananikulina00@mail.ru>
2024-06-21 23:50:08 +0300

    dz1

---
+++ toDoL/FileCache.swift
@@ -0,0 +1,60 @@
+//
+//  FileCache.swift
+//  toDoL
+//
+//  Created by snydia on 18.06.2024.
+//
+
+import Foundation
+
+final class FileCache {
+    private(set) var toDoItems: [TodoItem] = []
+    
+    private let fileName: String
+    private let fileManager: FileManager
+    
+    init(
+        fileName: String = "Default",
+        fileManager: FileManager = .default
+    ) {
+        self.fileName = fileName
+        self.fileManager = fileManager
+    }
+    
+    func addItem(_ item: TodoItem) {
+        guard !toDoItems.contains(where: { $0.id == item.id }) else { return }
+        toDoItems.append(item)
+    }
+    
+    @discardableResult
+    func deleteItem(with id: String) -> TodoItem? {
+        guard let index = toDoItems.firstIndex(where: { $0.id == id }) else { return nil }
+        return toDoItems.remove(at: index)
+    }
+    
+    @discardableResult
+    func saveAllItems() -> Bool {
+        guard let folder = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first else { return false }
+        let filePath = folder.appending(path: fileName)
+        
+        do {
+            let data = try JSONSerialization.data(withJSONObject: toDoItems.map { $0.json })
+            try data.write(to: filePath)
+            return true
+        } catch {
+            return false
+        }
+    }
+    
+    func loadItems() {
+        guard 
+            let folder = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first,
+            let data = try? Data(contentsOf: folder.appending(path: fileName)),
+            let rawValues = try? JSONSerialization.jsonObject(with: data) as? [[String: Any]]
+        else {
+            return
+        }
+        
+        toDoItems = rawValues.compactMap { TodoItem.parse(json: $0) }
+    }
+}
---
+++ toDoL/TodoItem.swift
@@ -0,0 +1,141 @@
+//
+//  TodoItem.swift
+//  toDoL
+//
+//  Created by snydia on 17.06.2024.
+//
+
+import Foundation
+
+struct TodoItem: Equatable {
+    enum Importance: String {
+        case unimportant
+        case usual
+        case important
+    }
+    
+    let id: String
+    let text: String
+    let importance: Importance
+    let deadline: Date?
+    let isCompleted: Bool
+    let creationDate : Date
+    let modificationDate : Date?
+    
+    init(
+        id: String = UUID().uuidString,
+        text: String,
+        importance: Importance,
+        deadline: Date? = nil,
+        isCompleted: Bool = false,
+        creationDate : Date = Date(),
+        modificationDate : Date? = nil
+    ) {
+        self.id = id
+        self.text = text
+        self.importance = importance
+        self.deadline = deadline
+        self.isCompleted = isCompleted
+        self.creationDate  = creationDate
+        self.modificationDate  = modificationDate
+    }
+}
+
+// MARK: - DTO
+
+extension TodoItem {
+    private enum CodingKeys {
+        static let id = "id"
+        static let text = "text"
+        static let importance = "importance"
+        static let deadline = "deadline"
+        static let isCompleted = "isCompleted"
+        static let creationDate = "creationDate"
+        static let modificationDate = "modificationDate"
+    }
+    
+    var json: Any {
+        var result: [String: Any] = [
+            CodingKeys.id: id,
+            CodingKeys.text: text,
+            CodingKeys.isCompleted: isCompleted,
+            CodingKeys.creationDate: creationDate.timeIntervalSince1970,
+        ]
+        
+        if importance != .usual {
+            result[CodingKeys.importance] = importance.rawValue
+        }
+        
+        if let deadline {
+            result[CodingKeys.deadline] = deadline.timeIntervalSince1970
+        }
+        
+        if let modificationDate {
+            result[CodingKeys.modificationDate] = modificationDate.timeIntervalSince1970
+        }
+        
+        return result
+    }
+    
+    static func parse(json: Any) -> TodoItem? {
+        guard
+            let data = json as? [String: Any],
+            let id = data[CodingKeys.id] as? String,
+            let text = data[CodingKeys.text] as? String,
+            let isCompleted  = data[CodingKeys.isCompleted] as? Bool,
+            let creationDateRawValue = data[CodingKeys.creationDate] as? TimeInterval
+        else {
+            return nil
+        }
+        
+        let importanceRawValue = data[CodingKeys.importance] as? String
+        let deadlineRawValue = data[CodingKeys.deadline] as? TimeInterval
+        let modificationDateRawValue = data[CodingKeys.modificationDate] as? TimeInterval
+        
+        return TodoItem(
+            id: id,
+            text: text,
+            importance: importanceRawValue.flatMap { Importance(rawValue: $0) } ?? .usual,
+            deadline: deadlineRawValue.map { Date(timeIntervalSince1970: $0) },
+            isCompleted: isCompleted,
+            creationDate: Date(timeIntervalSince1970: creationDateRawValue),
+            modificationDate: modificationDateRawValue.map { Date(timeIntervalSince1970: $0) }
+        )
+    }
+    
+    var csv: String {
+        let deadlineString = deadline?.timeIntervalSince1970.description ?? ""
+        let modificationDateString = modificationDate?.timeIntervalSince1970.description ?? ""
+        return "\(id),\(text),\(importance.rawValue),\(deadlineString),\(isCompleted),\(creationDate.timeIntervalSince1970),\(modificationDateString)"
+    }
+    
+    static func parse(csv: String) -> TodoItem? {
+        let components = csv.components(separatedBy: ",")
+        
+        guard
+            components.count == 7,
+            let isCompleted = Bool(components[4]),
+            let creationDateInterval = TimeInterval(components[5])
+        else {
+            return nil
+        }
+        
+        let id = components[0]
+        let text = components[1]
+        let importance = Importance(rawValue: components[2]) ?? .usual
+        let deadline = components[3].isEmpty ? nil : Date(timeIntervalSince1970: TimeInterval(components[3]) ?? 0)
+        let creationDate = Date(timeIntervalSince1970: creationDateInterval)
+        let modificationDate = components[6].isEmpty ? nil : Date(timeIntervalSince1970: TimeInterval(components[6]) ?? 0)
+        
+        return TodoItem(
+            id: id,
+            text: text,
+            importance: importance,
+            deadline: deadline,
+            isCompleted: isCompleted,
+            creationDate: creationDate,
+            modificationDate: modificationDate
+        )
+    }
+    
+}
